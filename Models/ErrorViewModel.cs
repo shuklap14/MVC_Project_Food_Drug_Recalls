e7a7fe57d0f0775a0a7dbfbf2cdf5aa09950f6ef
Pragati Brijesh Shukla <shuklap@usf.edu>
2019-11-21 17:56:23 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Project_Food_Drug_Recalls.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
